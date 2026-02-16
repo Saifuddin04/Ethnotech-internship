de112b0ad6279cc8b4ea904a700db8f0345fa07e
Saifuddin04 <saifuddinshariff176@gmail.com>
2026-02-16 16:32:02 +0530

    fixing render error
    (Background on this error at: https://sqlalche.me/e/20/e3q8)
    
    fixed this error

--- Blog-website/app.py
+++ Blog-website/app.py
@@ -133,5 +133,8 @@ def logout():
 def inject_categories():
     return dict(Category=Category)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
